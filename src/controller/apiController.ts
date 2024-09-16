c83fca964ba453e861b623ddef19fb1853c2a0de
saifulislam-bd <saifulislam.github@gmail.com>
2024-09-16 20:42:29 +0600

    feat: Add email functionality in register

--- src/controller/apiController.ts
+++ src/controller/apiController.ts
@@ -1,5 +1,3 @@
- 
- 
 import { NextFunction, Request, Response } from 'express'
 import httpResponse from '../util/httpResponse'
 import responseMessage from '../constant/responseMessage'
@@ -9,6 +7,9 @@ import { IRegisterRequestBody, IUser } from '../types/userType'
 import { validateJoiSchema, validateRegisterBody } from '../service/validationService'
 import databaseService from '../service/databaseService'
 import { EUserRole } from '../constant/userConstant'
+import config from '../config/config'
+import emailService from '../service/emailService'
+import logger from '../util/logger'
 
 interface IRegisterRequest extends Request {
     body: IRegisterRequestBody
@@ -44,18 +45,18 @@ export default {
             
         // TODO: Add Register logic
         // * Body validation
-        const {error, value} = validateJoiSchema<IRegisterRequestBody>(validateRegisterBody, body);
+            const {error, value} = validateJoiSchema<IRegisterRequestBody>(validateRegisterBody, body);
 
-        if(error) {
-            return httpError(next, error, req, 422)
-        }
-            // * Phone no parsing and validation
+            if(error) {
+                return httpError(next, error, req, 422)
+            }
+        // * Phone no parsing and validation
             const {phoneNo,name, consent, email, password} = value;
             const {countryCode, internationalNumber, isoCode} = quicker.parsePhoneNumber('+' + phoneNo);
 
-        if(!countryCode || !isoCode || !internationalNumber) {
-            return httpError(next, new Error(responseMessage.INVALID_PHONE_NUMBER), req, 422)
-        }  
+            if(!countryCode || !isoCode || !internationalNumber) {
+                return httpError(next, new Error(responseMessage.INVALID_PHONE_NUMBER), req, 422)
+            }  
 
         // * Timzezone
           const timezone =  quicker.countryTimezone(isoCode);
@@ -64,46 +65,58 @@ export default {
           }
           
          // * Check user existence using email address
-        const user = await databaseService.findUserByEmail(email)
-        if (user) {
-            return httpError(next, new Error(responseMessage.ALREADY_EXIST('user', email)), req, 422)
-        }
+            const user = await databaseService.findUserByEmail(email)
+            if (user) {
+                return httpError(next, new Error(responseMessage.ALREADY_EXIST('user', email)), req, 422)
+            }
         
         // * Encrypt password
-        const encryptedPassword = await quicker.hashPassword(password);
+            const encryptedPassword = await quicker.hashPassword(password);
 
         // * Account confirmation object data
-        const token = quicker.generateRandomId();
-        const code = quicker.generateOtp(6);
+            const token = quicker.generateRandomId();
+            const code = quicker.generateOtp(6);
 
         // * Create user
-        const payload: IUser = {
-        name,
-        email,
-        password: encryptedPassword,
-        phoneNo:{
-            countryCode,
-            isoCode,
-            internationalNumber
-        },
-        accountConfirmation: {
-            status: false,  
-            token,
-            code,
-            timestamp: null
-        },
-        passwordReset: {
-            token: null,
-            expiry: null,
-            lastResetAt: null
-        },
-        lastLoginAt:  null,
-        role: EUserRole.USER,
-        timezone: timezone[0].name,
-        consent
-        }
-        const newUser = await databaseService.registerUser(payload)
+            const payload: IUser = {
+            name,
+            email,
+            password: encryptedPassword,
+            phoneNo:{
+                countryCode,
+                isoCode,
+                internationalNumber
+            },
+            accountConfirmation: {
+                status: false,  
+                token,
+                code,
+                timestamp: null
+            },
+            passwordReset: {
+                token: null,
+                expiry: null,
+                lastResetAt: null
+            },
+            lastLoginAt:  null,
+            role: EUserRole.USER,
+            timezone: timezone[0].name,
+            consent
+            }
+            const newUser = await databaseService.registerUser(payload);
+
         // * Send E-mail
+            const confirmationUrl = `${config.FRONTEND_URL}/confirmation/${token}?code=${code}`;
+            const to = [email];
+            const subject = 'Confirm Your Account';
+            const text = `Hey ${name}, Please confirm your account by clicking on the link given below:\n\n${confirmationUrl}`;
+
+            emailService.sendEmail(to,subject,text).catch((err)=>{
+                logger.error('EMAIL_SERVICE',{
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                    meta: err
+                })
+            })
             httpResponse(req, res, 201, responseMessage.SUCCESS, {_id:newUser._id})
         } catch (err) {
             httpError(next, err, req, 500)
---
+++ src/service/emailService.ts
@@ -0,0 +1,21 @@
+import { Resend } from 'resend';
+import config from '../config/config';
+
+const resend = new Resend(config.EMAIL_SERVICE_API_KEY);
+
+export default {
+  sendEmail: async(to:string[], subject: string, text: string)=>{
+    try {
+      await resend.emails.send(
+        {
+          from:`Saiful Islam <onboarding@resend.dev>`,
+          to,
+          subject,
+          text
+        }
+      )
+    } catch (error) {
+      throw error
+    }
+  }
+}
